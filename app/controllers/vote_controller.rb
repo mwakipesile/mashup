3145e621837426f0799874471f86086a0dcd27a3
M. Edgar joel <mkakajoel@gmail.com>
2016-12-03 10:32:22 -0500

    moved matchup routes to contest controller

--- app/controllers/vote_controller.rb
+++ app/controllers/vote_controller.rb
@@ -1,15 +1,4 @@
 class VoteController < ApplicationController
-  get '/matchup' do
-    user_id = session[:username] || request.ip
-    @image_id, @image_id2 = Matchup.pair(user_id)
-    redirect('/no_matchups') if @image_id.nil? || @image_id2.nil?
-
-    @image, @image2 = Image.fetch(@image_id, @image_id2)
-    @rating, @rating2 = Rating.fetch(@image_id, @image_id2)
-
-    erb :matchup
-  end
-
   post '/vote' do
     winner_id = params[:winner]
     loser_id = params[:loser]
